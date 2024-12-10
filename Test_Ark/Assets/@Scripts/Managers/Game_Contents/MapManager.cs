6a77a97a1d3f387d06175044d96ca32842a23871
Hyun <hnjogo@gmail.com>
2024-12-10 19:12:10 +0900

    temp 코드

--- Test_Ark/Assets/@Scripts/Managers/Game_Contents/MapManager.cs
+++ Test_Ark/Assets/@Scripts/Managers/Game_Contents/MapManager.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class MapManager
 {
     // Grid 방식으로 체크 (Cell Pos)
+    // 생각해볼 점
+    // Map 을 직접 랜덤으로 만들고 싶음
+    // 그런데 이 방식은 프리팹으로 만드는 게 맞나?
+    // - 프리팹을 만들고 그 안에서 TileManager 등을 넣어두고
+    // 해당 매니저가 타일을 랜덤으로 배치하도록 한다
+    // - 그리고 이 Map Manager에서는 오브젝트들의 이동 및 상호작용을 우선 (타일 데이터가 필요하면 TileManager에게)
+    //
 
     public GameObject Map {  get; private set; }
     public string MapName { get; private set; }
     public Grid CellGrid { get; private set; }
 
+    public void LoadMap(string mapName)
+    {
 
+    }
 }
